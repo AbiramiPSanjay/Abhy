b0ef7d252293ce75e1b650d515d5df4a09a10888
AbiramiPSanjay <abiramigparamasivam@gmail.com>
2025-02-12 18:49:56 +0530

    BitwiseOperator.py

---
+++ BitwiseOperator.py
@@ -0,0 +1,8 @@
+a=int(input("Enter 1st Number")
+b=int(input("Enter 2nd Number")
+print("Bitwise AND",a&b)
+print("Bitwise OR",a|b)
+print("Bitwise XOR",a^b)  
+print("Bitwise NOT",~b)  
+print("Bitwise Left shift",a<<b)
+print("Bitwise Right shift",a>>b)       
