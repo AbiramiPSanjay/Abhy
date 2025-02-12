b15a7c9e77fa4ca251ba246d3ea62cd531e29c8f
AbiramiPSanjay <abiramigparamasivam@gmail.com>
2025-02-12 18:45:03 +0530

    LogicalOperator.py

---
+++ LogicalOperator.py
@@ -0,0 +1,14 @@
+a=int(input("Enter 1st Number:")
+b=int(input("Enter 2nd Number:")
+if a>80 and b>60:
+      print("True")
+else:
+     print("False")
+if a>80 or b>60:
+      print("True")
+else:
+     print("False")
+if not a>80:
+      print("True")
+else:
+     print("False")
