a2d15b17563e1310c0085673816f729e77e221ea
AbiramiPSanjay <abiramigparamasivam@gmail.com>
2025-02-14 18:19:01 +0530

    markguide.py

---
+++ markguide.py
@@ -0,0 +1,13 @@
+m=int(input("Enter the number:")
+if (m>90):
+      print("Grade A")
+elif(m>=80)&&(m<89):
+      print("Grade B")
+elif(m>=70)&&(m<79):
+      print("Grade C")
+elif(m>=60)&&(m<69):
+      print("Grade D")
+elif(m>=50)&&(m<59):
+      print("Grade E")
+else:
+      print("FAIL")
