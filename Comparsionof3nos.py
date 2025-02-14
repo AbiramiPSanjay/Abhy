983527ff2a235c20b7edde00af7d06716e7a3ad4
AbiramiPSanjay <abiramigparamasivam@gmail.com>
2025-02-14 18:32:17 +0530

    Comparsionof3nos.py

---
+++ Comparsionof3nos.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the 1st number:")
+b=int(input("Enter the 2nd number:")
+c=int(input("Enter the 3rd number:")      
+if(a>b)&&(a>c):
+      print(a,"is greater than",b,"&",c)
+elif(b>c):
+      print(b,"is greater than",a,"&",c)
+else:
+      print(c,"is greater than",a,"&",b)
