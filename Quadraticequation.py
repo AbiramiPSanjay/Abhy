d3258eea326705c04b0c0818a2bec6884d641c98
AbiramiPSanjay <abiramigparamasivam@gmail.com>
2025-02-14 18:05:17 +0530

    Quadraticequation.py

---
+++ Quadraticequation.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the 1st number:")
+b=int(input("Enter the 2nd number:") 
+c=int(input("Enter the 3rd number:")
+d=(b**)-(4*a*c)
+x1=(-b+d)/(2a)
+x2=(-b-d)/(2a)
+print("Factors are",x1,x2)
+      
