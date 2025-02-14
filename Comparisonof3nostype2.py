9d17847cef4bd35ea5c1da854c4d939563dccb1c
AbiramiPSanjay <abiramigparamasivam@gmail.com>
2025-02-14 18:34:34 +0530

    Comparisonof3nostype2.py

---
+++ Comparisonof3nostype2.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the 1st number:")
+b=int(input("Enter the 2nd number:")
+c=int(input("Enter the 3rd number:")      
+if(b>c):
+      if(a>b):
+              print(a,"is greater than",b,"&",c)
+      else:
+           print(b,"is greater than",a,"&",c)
+else:
+     print(c,"is greater than",a,"&",b)
