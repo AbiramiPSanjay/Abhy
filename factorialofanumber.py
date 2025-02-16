735bad96728df83ecb1ff213067935b56ec1c703
AbiramiPSanjay <abiramigparamasivam@gmail.com>
2025-02-16 19:33:20 +0530

    factorialofanumber.py

---
+++ factorialofanumber.py
@@ -0,0 +1,12 @@
+n=int(input("Enter the value of n:"))
+i=1
+Factorial=1
+if n<1:
+  print("Enter +ve Number")
+elif(n==0):
+  print("Factorial of 0 is 1")
+else:
+  while(i<n)
+     Factorial=Factorial*1
+     i=i+1
+  Print(Factorial) 
