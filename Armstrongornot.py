2fe4f395c7054cad4ae58662cfbd5a13dde296e7
AbiramiPSanjay <abiramigparamasivam@gmail.com>
2025-02-16 19:46:44 +0530

    Armstrongornot.py

---
+++ Armstrongornot.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the number:")
+n=org
+sum=0
+While(n>0)
+    a=n%10
+    sum=sum+a*a*a
+    n=n//10
+if(sum==org):
+    print("Armstrong Number")
+else:
+    print("Not Armstrong Number")
